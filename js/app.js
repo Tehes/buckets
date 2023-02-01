328c4fd4d279b3c8990cd7a1b1b090743f5c098a
Tino Sabetta <tino.sabetta@gmail.com>
2023-02-01 18:52:34 +0100

    loaded data for the first time

--- js/app.js
+++ js/app.js
@@ -10,23 +10,50 @@ async function fetchStats() {
 /* --------------------------------------------------------------------------------------------------
 Variables
 ---------------------------------------------------------------------------------------------------*/
- const leftCard = document.querySelector(".left");
- leftCard.team = leftCard.querySelector("h2");
- leftCard.setBgColor = function(value) {
-    document.body.style.setProperty("--bg-left", "var(--"+ value +")");
- }
+const leftCard = document.querySelector(".left");
+leftCard.team = leftCard.querySelector("h2");
+leftCard.setBgColor = function (value) {
+    document.body.style.setProperty("--bg-left", `var(--${value})`);
+}
+leftCard.setLogo = function (value) {
+    document.body.style.setProperty("--bg-img-left", `url(../img/${value}.svg)`);
+}
+leftCard.playerImg = leftCard.querySelector("img");
+leftCard.playerName = leftCard.querySelector("h1");
 
 /* --------------------------------------------------------------------------------------------------
 functions
 ---------------------------------------------------------------------------------------------------*/
 
+String.prototype.shuffle = function () {
+    var chars = this.split("");
+    var charsAmount = chars.length;
+
+    for (var i = charsAmount - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+    return chars.join("");
+};
 
 function init() {
-    document.addEventListener("touchstart", function() {}, false);
+    document.addEventListener("touchstart", function () { }, false);
     fetchStats().then(
-        function(data) {console.log(data[0]);}
-      );
-    console.log(leftCard.team);
+        function (data) {
+            data.sort((a, b) => 0.5 - Math.random());
+
+            leftCard.setBgColor(data[0].TEAM);
+            leftCard.setLogo(data[0].TEAM);
+            leftCard.team.textContent = data[0].TEAM;
+            leftCard.playerImg.src = data[0].pic;
+            leftCard.playerName.textContent = data[0].Player;
+            console.log(data[0]);
+        }
+    );
+
+
 }
 
 /* --------------------------------------------------------------------------------------------------
